f8dc73c7c506db1bceeb1fb69ef11bcba801c58e
yoshiharahyui <1015hyui@gmail.com>
2024-06-20 13:18:21 +0900

    first commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by 吉原飛偉 on 2024/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
